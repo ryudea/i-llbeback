2504512d809ca389ab091fa896b355349f8a3e31
pc04 <pc04@pc04-PC>
2018-11-28 20:21:24 +0900

    불필요한 에러잡음

--- src/Test/CardTest.java
+++ src/Test/CardTest.java
@@ -2,57 +2,57 @@ package Test;
 
 public class CardTest {
 	public static void main(String[] args) {
-		// Card class의 넓이를 출력하세요 // 100
-		System.out.println(Card.width);
-		// Card class의 높이를 출력하세요.// 250
-		System.out.println(Card.height);
-		// Card class의 객체를 만들어주세요 : 변수명 cd1
-		Card cd1 = new Card();
-		// cd1의 분양을 spade로 변경해주세요. // cd1.kind = "SPADE"
-		cd1.kind = "SPADE";
-		// cd1의 숫자를 7로 변경해주세요 // cd1.number = 7
-		cd1.number = 7;
-		// Card 클레스의 객체를 만들어주세요 : 변수명 cd2
-		Card cd2 = new Card();
-		// cd2의 문양을 HEART로 변경해주세요 // cd2 = "HEART"
-		cd2.kind = "HEART";
-		// cd2의 값을 3으로 변경해주세요. // cd2 = 3
-		cd2.number = 3;
-		// 아래와 같이 출력해주세요.ce
-		// cd1의 모양 SPADE숫자 : 7 너비 : 100 높이 : 250
-		// cd2의 모양 HEART숫자 : 3 너비 : 100 높이 : 250
-		System.out.println("cd1의 모양 " + cd1.kind + "숫자 : " + cd1.number + " 너비 : " + cd1.width + " 높이 : " + cd1.height);
-		System.out.println("c   d2의 모양 " + cd2.kind + "숫자 : " + cd2.number + " 너비 : " + cd2.width + " 높이 : " + cd2.height);
-		// cd1의 너비를 50으로 변경 // cd1.width = 50 -> 클레스를 불러오는 거기 때문에 전체가 바뀐다/
-		cd1.width = 50; 
-		// cd1의 높이를 80으로 변경// cd2.height = 80 - >  sameThings
-		cd1.height = 80;
-		// 아래와 같이 출력해주세요.
-		// cd1의 모양 SPADE숫자 : 7 너비 : 50 높이 : 80
-		// cd2의 모양 HEART숫자 : 3 너비 : 50 높이 : 80
-		System.out.println("cd1의 모양 " + cd1.kind + "숫자 : " + cd1.number
-				+ " 너비 : " + cd1.width + " 높이 : " + cd1.height);
-		System.out.println("cd2의 모양 " + cd2.kind + "숫자 : " + cd2.number
-				+ " 너비 : " + cd2.width + " 높이 : " + cd2.height);
-		
-	}
-}
-
-class Card {
-
-	// 인스턴스 변수
-	String kind;
-	int number;
-
-	// 클레스 변수
-	static int width = 100;
-	static int height = 250;
-
-	void method() {
-		int v = 3;// 지역변수-method 안에서 생성된 변수- , 인스턴스 변수
-	}
-
-	static void method2() {
-
+//		// Card class의 넓이를 출력하세요 // 100
+//		System.out.println(Card.width);
+//		// Card class의 높이를 출력하세요.// 250
+//		System.out.println(Card.height);
+//		// Card class의 객체를 만들어주세요 : 변수명 cd1
+//		Card cd1 = new Card();
+//		// cd1의 분양을 spade로 변경해주세요. // cd1.kind = "SPADE"
+//		cd1.kind = "SPADE";
+//		// cd1의 숫자를 7로 변경해주세요 // cd1.number = 7
+//		cd1.number = 7;
+//		// Card 클레스의 객체를 만들어주세요 : 변수명 cd2
+//		Card cd2 = new Card();
+//		// cd2의 문양을 HEART로 변경해주세요 // cd2 = "HEART"
+//		cd2.kind = "HEART";
+//		// cd2의 값을 3으로 변경해주세요. // cd2 = 3
+//		cd2.number = 3;
+//		// 아래와 같이 출력해주세요.ce
+//		// cd1의 모양 SPADE숫자 : 7 너비 : 100 높이 : 250
+//		// cd2의 모양 HEART숫자 : 3 너비 : 100 높이 : 250
+//		System.out.println("cd1의 모양 " + cd1.kind + "숫자 : " + cd1.number + " 너비 : " + cd1.width + " 높이 : " + cd1.height);
+//		System.out.println("c   d2의 모양 " + cd2.kind + "숫자 : " + cd2.number + " 너비 : " + cd2.width + " 높이 : " + cd2.height);
+//		// cd1의 너비를 50으로 변경 // cd1.width = 50 -> 클레스를 불러오는 거기 때문에 전체가 바뀐다/
+//		cd1.width = 50; 
+//		// cd1의 높이를 80으로 변경// cd2.height = 80 - >  sameThings
+//		cd1.height = 80;
+//		// 아래와 같이 출력해주세요.
+//		// cd1의 모양 SPADE숫자 : 7 너비 : 50 높이 : 80
+//		// cd2의 모양 HEART숫자 : 3 너비 : 50 높이 : 80
+//		System.out.println("cd1의 모양 " + cd1.kind + "숫자 : " + cd1.number
+//				+ " 너비 : " + cd1.width + " 높이 : " + cd1.height);
+//		System.out.println("cd2의 모양 " + cd2.kind + "숫자 : " + cd2.number
+//				+ " 너비 : " + cd2.width + " 높이 : " + cd2.height);
+//		
+//	}
+//}
+//
+//class Card {
+//
+//	// 인스턴스 변수
+//	String kind;
+//	int number;
+//
+//	// 클레스 변수
+//	static int width = 100;
+//	static int height = 250;
+//
+//	void method() {
+//		int v = 3;// 지역변수-method 안에서 생성된 변수- , 인스턴스 변수
+//	}
+//
+//	static void method2() {
+//
 	}
 }
